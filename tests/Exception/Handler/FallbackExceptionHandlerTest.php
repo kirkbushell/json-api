26270f32763ae76afd94a04bfb8cb1fd84f2bad3
Kirk Bushell <torm3nt@gmail.com>
2015-10-23 15:53:24 +0100

    Added status code check for response bag

--- tests/Exception/Handler/FallbackExceptionHandlerTest.php
+++ tests/Exception/Handler/FallbackExceptionHandlerTest.php
@@ -3,6 +3,7 @@ namespace Tobscure\Tests\Exception\Handler;
 
 use Exception;
 use Tobscure\JsonApi\Exception\Handler\FallbackExceptionHandler;
+use Tobscure\JsonApi\Exception\Handler\ResponseBag;
 
 class FallbackExceptionHandlerTest extends \PHPUnit_Framework_TestCase
 {
@@ -11,5 +12,14 @@ class FallbackExceptionHandlerTest extends \PHPUnit_Framework_TestCase
         $handler = new FallbackExceptionHandler(false);
 
         $this->assertTrue($handler->manages(new Exception));
+    }
+
+    public function testErrorHandlingWithoutDebugMode()
+    {
+        $handler = new FallbackExceptionHandler(false);
+        $response = $handler->handle(new Exception);
+
+        $this->assertInstanceOf(ResponseBag::class, $response);
+        $this->assertEquals(500, $response->getStatus());
     }
 }
